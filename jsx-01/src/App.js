0957f67b3cfff5881c515976a16ae7c6c4874779
marcobonni <marco.bonni19@gmail.com>
2023-02-23 12:39:41 +0100

    first try

--- jsx-01/src/App.js
+++ jsx-01/src/App.js
@@ -1,10 +1,11 @@
 import React from "react"
-import Hello from "./Hello"
+// import Hello from "./Hello"
+import Welcome from "../../my_app/src/Welcome"
 export class App extends React.Component {
     render() {
         return (
         <div>
-            <Hello/>
+            <Welcome name="Marco"/>
         </div>
         )
     }
