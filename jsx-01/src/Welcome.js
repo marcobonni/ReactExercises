80444be8ec428bacb03b36ac144e352515d7aeb7
marcobonni <marco.bonni19@gmail.com>
2023-02-28 01:25:57 +0100

    second try

--- jsx-01/src/Welcome.js
+++ jsx-01/src/Welcome.js
@@ -4,7 +4,7 @@ export class Welcome extends React.Component {
     render() {
         return (
             <div>
-                {!!this.props.name && <p>Welcome {this.props.name}!</p>}
+                {!!this.props.name && <p><strong>Welcome {this.props.name}!</strong></p>}
                 {!this.props.name && <p>Welcome User!</p>}
                 <Age/>
             </div>
