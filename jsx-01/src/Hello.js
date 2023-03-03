f0de6c47444ce233aba1ddab5b7a4f65c9200d7e
marcobonni <marco.bonni19@gmail.com>
2023-03-03 15:11:07 +0100

    first try

--- jsx-01/src/Hello.js
+++ jsx-01/src/Hello.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Age } from "./Age"
+import  Age  from "./Age"
 import Message from "./Message"
 export class Hello extends React.Component {
     render() {
@@ -7,7 +7,7 @@ export class Hello extends React.Component {
             <div>
                 <h1>Hello</h1>
                 <Message/>
-                <Age age=18 />
+                <Age age={18} />
             </div>
         )
     }
--- jsx-01/src/Welcome.js
+++ jsx-01/src/Welcome.js
@@ -1,13 +1,14 @@
 import React from "react";
 
-
 export class Welcome extends React.Component {
     render() {
-        return (
-        <div>
-            {!!this.props.name && <p>Welcome {this.props.name}!</p>}
-            {!this.props.name && <p>Welcome User!</p>}
-        </div>
-        )
+        if (this.props.age > 17) {
+            return (
+                <div>
+                    {!!this.props.name && <p>Welcome {this.props.name}!</p>}
+                    {!this.props.name && <p>Welcome User!</p>}
+                </div>
+            )
+        }
     }
 }
