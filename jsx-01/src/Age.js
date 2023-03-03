26100e4b31223d0bcb6d4875c6d496c496548689
marcobonni <marco.bonni19@gmail.com>
2023-03-03 15:54:38 +0100

    first try

--- jsx-01/src/Age.js
+++ jsx-01/src/Age.js
@@ -3,6 +3,11 @@ import React from "react";
 
 export class Age {
     render() {
-        return <p>Your age is: {this.props.age} </p>
+        if (this.props.age > 17) {
+            return <p>Your age is: {this.props.age} </p>
+        }
+        else {
+            return <p>You are very young!</p>
+        }
     }
 }
