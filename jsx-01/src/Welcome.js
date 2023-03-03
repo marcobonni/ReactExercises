cf5942b2a9cbe2a3cad2fbbf1af9575234da36ba
marcobonni <marco.bonni19@gmail.com>
2023-03-03 15:19:05 +0100

    first try

--- jsx-01/src/Welcome.js
+++ jsx-01/src/Welcome.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { Age } from "./Age";
 
 export class Welcome extends React.Component {
     render() {
-        if (this.props.age > 17) {
+        if (this.props.age > 17 && !this.props.age === undefined) {
             return (
                 <div>
                     {!!this.props.name && <p>Welcome {this.props.name}!</p>}
                     {!this.props.name && <p>Welcome User!</p>}
+                    <Age />
                 </div>
             )
         }
