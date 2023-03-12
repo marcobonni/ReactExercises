159ff7e93f8979e0e05ddefd717ca8f6d262c439
marcobonni <marco.bonni19@gmail.com>
2023-03-12 20:15:26 +0100

    first try

--- jsx-01/src/Login.js
+++ jsx-01/src/Login.js
@@ -5,17 +5,20 @@ class Login extends React.Component {
         username: "",
         password: "",
         checked: "",
+        disabled: true,
     }
     HandleUserNameChange = (event) => {
         const value = event.target.value
         this.setState({
             username: value,
+            disabled: false,
         })
     } 
     HandlePasswordChange = (event) => {
         const value = event.target.value
         this.setState({
             password: value,
+            disabled: false,
         })
     }
     HandleCheckboxChange = (event) => {
@@ -24,26 +27,34 @@ class Login extends React.Component {
             checked: value,
         })
     }
+    HandleOnLogin = (username, password) => {
+        const { onLogin } = this.props;
+        onLogin({ username, password});
+    }
     render() {
         return (
-            <div>
+            <div className="form">
+                <h2>Username</h2>
                 <input
                     type="text"
                     name="username"
                     onChange={this.HandleUserNameChange}
                     value={this.state.username}
                 />
+                <h2>password</h2>
                 <input type="password"
                     name="password"
                     onChange={this.HandlePasswordChange}
                     value={this.state.password}
                 />
+                <h2>remember</h2>
                 <input
                     type="checkbox"
                     name="remember"
                     onChange={this.HandleCheckboxChange}
                     value={this.state.checked}
                 />
+                <button disabled={this.state.disabled} onClick={this.HandleOnLogin}>Login</button>
             </div>
         )
     }
