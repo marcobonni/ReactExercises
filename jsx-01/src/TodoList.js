fe393ab9dff6b83cbc25d47318e3614aed2f4ccc
marcobonni <marco.bonni19@gmail.com>
2023-03-16 14:28:35 +0100

    switch

---
+++ jsx-01/src/TodoList.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+class MyTodo extends React.Component {
+    render() {
+        return (
+            <ul>
+                <li></li>
+            </ul>
+        )
+    }
+}
