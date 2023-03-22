2fb076c7c7203daa03db3cbb34ee788c414a0266
marcobonni <marco.bonni19@gmail.com>
2023-03-22 16:40:30 +0100

    fix typo

--- jsx-01/src/TodoList.js
+++ jsx-01/src/TodoList.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React  from 'react';
 
-class TodoList extends Component {
+class TodoList extends React.Component {
   constructor(props) {
     super(props);
     this.input = React.createRef();
