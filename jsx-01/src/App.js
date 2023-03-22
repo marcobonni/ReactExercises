918ded224d2bdadbc02e37ef9d32e4dc89538873
marcobonni <marco.bonni19@gmail.com>
2023-03-22 19:08:31 +0100

    first try

--- jsx-01/src/App.js
+++ jsx-01/src/App.js
@@ -1,20 +1,12 @@
 import React from "react"
-import ClickCounter from "./ClickCounter"
-import ClickTracker from "./ClickTracker"
-import InteractiveWelcome from "./interactiveWelcome"
-import Login2 from "./Login2"
-import TodoList from "./TodoList"
+import Login from "./Login"
 class App extends React.Component {
 
     render() {
         return (
-        <div>
-            <ClickCounter/>
-            <ClickTracker/>
-            <InteractiveWelcome/>
-            <Login2/>       
-            <TodoList/>
-        </div>
+            <div>
+                <Login/>
+            </div>
         )
     }
 }
--- jsx-01/src/Login.js
+++ jsx-01/src/Login.js
@@ -1,102 +1,53 @@
-import React, { createRef } from "react";
+import React from "react";
 
 class Login extends React.Component {
-    _formRef = createRef()
-    // state = {
-    //     username: "",
-    //     password: "",
-    //     checked: "",
-    //     disabled: true,
-    // }
-    // HandleUserNameChange = (event) => {
-    //     const value = event.target.value
-    //     this.setState({
-    //         username: value,
-    //         disabled: false,
-    //     })
-    // }
-    // HandlePasswordChange = (event) => {
-    //     const value = event.target.value
-    //     this.setState({
-    //         password: value,
-    //         disabled: false,
-    //     })
-    // }
-    // HandleCheckboxChange = (event) => {
-    //     const value = event.target.value
-    //     this.setState({
-    //         checked: value,
-    //     })
-    // }
-    // HandleOnLogin = (username, password) => {
-    //     const { onLogin } = this.props;
-    //     onLogin({ username, password });
-    // }
-    HandleReset = () => {
-        this.setState({
-            username: "",
-            password: "",
-            checked: "",
-            disabled: true
-        })
-    }
-    handleFormSubmit = (event) => {
-        event.preventDefault()
-        const username = event.target.elements.username.value
-        const password = event.target.elements.password.value
-        const remember = event.target.elements.remember.checked
-
-        console.log(
-            username, 
-            password,
-            remember
-        )
+    state = {
+        username: "",
+        password: "",
+        remember: false,
     }
     HandleInput = (event) => {
-        const value = event.target.elements.username.value
-        const name = event.target.name
-        const type = event.target.type
-        const checked = event.target.checked
+        const {name, type, value, checked} = event.target
+        const v = type === "checkbox" ? checked : value;
         this.setState({
-            [name]: type === "checkbox" ? checked : value,
-            [name]: type === "disabled" ? false : true,
+            [name] : v,
         })
     }
+
     render() {
+        const LoginStyle = {
+            "background-color" : this.state.password.length < 8 ? "red" : "green"
+        }
         return (
-            <form ref={this._formRef} className="form" onSubmit={this.handleFormSubmit}>
+            <div className="form">
                 <h2>Username</h2>
                 <input
-                    defaultValue= ""
-                    ref={this.myref}
                     type="text"
                     name="username"
                     onChange={this.HandleInput}
                     value={this.state.username}
                 />
                 <h2>password</h2>
-                <input
-                    defaultValue=""
-                    ref={this.myref}
-                    type="password"
+                <input type="password"
                     name="password"
                     onChange={this.HandleInput}
                     value={this.state.password}
                 />
                 <h2>remember</h2>
                 <input
-                    defaultChecked=""
-                    ref={this.myref}
                     type="checkbox"
                     name="remember"
                     onChange={this.HandleInput}
-                    checked={this.state.checked}
                 />
-                <button type="disabled">Login</button>
-                <button>Reset</button>
-                <button onSubmit={this.handleFormSubmit}></button>
-                
-            </form>
+                <button 
+                    style={LoginStyle}
+                    disabled={
+                        this.state.username === "" || this.state.password === "" 
+                        ? true
+                        : false 
+                    }
+                    onClick={ () => this.props.onLogin(this.state)}>Login</button>
+            </div>
         )
     }
 }
