1de989616930ce120ea33bd827c6af77d9633b9d
marcobonni <marco.bonni19@gmail.com>
2023-03-24 17:29:12 +0100

    first try

--- jsx-01/src/App.js
+++ jsx-01/src/App.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Login from "./Login"
+import  Container  from "./Container"
 class App extends React.Component {
 
     render() {
         return (
             <div>
+                <Container />
                 <Login/>
             </div>
         )
