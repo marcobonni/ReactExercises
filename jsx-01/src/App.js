1547275dd36db82ef97d3077297ce7f34736b2d4
marcobonni <marco.bonni19@gmail.com>
2023-03-26 19:23:51 +0200

    firsy try

--- jsx-01/src/App.js
+++ jsx-01/src/App.js
@@ -1,31 +1,10 @@
 import React from "react";
-import { LanguageContext } from "./LanguageContext";
 import { Sum } from "./Sum";
-import Welcome from "./Welcome";
-import {Welcome2} from "./Welcome2"
-class App extends React.Component {
-    state = {
-        language: "en"
-    }
 
-    handleLanguageChange = (event) => {
-        this.setState({
-            language: event.target.value
-        })
-    }
+class App extends React.Component {
     render() {
         return (
-            <div>
-                <select value={this.state.language} onChange={this.handleLanguageChange}>
-                    <option value="en">English</option>
-                    <option value="it">Italiano</option>
-                </select>
-                <LanguageContext.Provider value={this.state.language}>
-                <Welcome/>
-                <Welcome2 name="Marco"/>
-                <Sum numbers={[0,1,2,3,4,5]}/>
-                </LanguageContext.Provider>
-            </div>
+                <Sum numbers={[0,1,2]}/>
         )
     }
 }
--- jsx-01/src/Sum.js
+++ jsx-01/src/Sum.js
@@ -1,19 +1,19 @@
-export function Sum(props) {
-    const numbers = props.numbers
+export function Sum({numbers = 0}) {
     const sum = numbers.reduce((a, b) => a + b, 0)
     const listItems = numbers.map((number, index) => (
         <span>
             {number}
             {index === numbers.length - 1 ? "" : " + "}
         </span>
-        ));
+    ));
     return (
         <div>
-            <h1>
-                <ul>
-                    {listItems} = {sum}
-                </ul>
-            </h1>
+                <h1>
+                    <ul>
+
+                        {!numbers.length === 0 && <h2>{listItems} = {sum}</h2>}
+                    </ul>
+                </h1>
         </div>
     )
 }
