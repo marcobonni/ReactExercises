d4c228b6bcd0d74e39fbca9e0ada13c3132e4e2d
marcobonni <marco.bonni19@gmail.com>
2023-03-26 17:11:53 +0200

    first try

--- jsx-01/src/Welcome.js
+++ jsx-01/src/Welcome.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { LanguageContext } from "./LanguageContext";
+const Strings = {
+    en: {
+        CURRENT_WELCOME: "Welcome"
+    },
+    it: {
+        CURRENT_WELCOME: "Benvenuto"
+    }
+}
 export class Welcome extends React.Component {
     render() {
             return (
                 <div>
-                    <p>Welcome, {this.props.nome}</p>
+                    <LanguageContext.Consumer>
+                    {(language) => {
+                        return(
+                            <div>{Strings[language].CURRENT_WELCOME}
+                            </div>
+                        )
+                    }}
+                    </LanguageContext.Consumer>
                 </div>
             )
 }
