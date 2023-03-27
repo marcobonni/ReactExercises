787831fcd1847cc20a1542c04ee1dd6a515bf087
marcobonni <marco.bonni19@gmail.com>
2023-03-27 04:49:04 +0200

    wtf

--- jsx-01/src/App.js
+++ jsx-01/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import {LoginFunc} from "./LoginFunc"
+import {Counter} from "./Counter"
 class App extends React.Component {
     render() {
         return (
             <div>
                 <LoginFunc/>
+                <Counter/>
             </div>
         )
     }
--- jsx-01/src/Counter.js
+++ jsx-01/src/Counter.js
@@ -1,10 +1,16 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export function Counter({ initialValue = 0}) {
     const [data, setData] = useState(initialValue)
     function HandleIncrement(data) {
         setData(data => data + 1)
     }
+    function OnCounterChange(data) {
+        console.log(`counter changed to ${data}`)
+    }
+    useEffect(() => {
+        OnCounterChange(data)
+    }, [data])
     return (
         <div>
             <h2>Count : {data}</h2>
