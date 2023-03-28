d8ad4876d328882d4848442c8d70729f94785dd1
marcobonni <marco.bonni19@gmail.com>
2023-03-28 22:25:58 +0200

    first try

--- jsx-01/src/App.js
+++ jsx-01/src/App.js
@@ -6,6 +6,7 @@ import {Welcome} from "./Welcome"
 import { Routes, Route } from "react-router-dom"; 
 import { NewCounter } from "./NewCounter";
 import { GithubParameter } from "./GitHub3";
+import { Link } from "react-router-dom";
 function App (){
     // const [showCounter, setShowCounter] = useState(true)
 
@@ -14,10 +15,15 @@ function App (){
     // }
         return (
             <div>
+                    <Link to="/:name">welcome</Link>
+                    <Link to="/counter">Counter</Link>
+                    <Link to="users/:username">Github</Link>
+                    <Link to="/">Back Home</Link>
                 <Routes>
+                    
                     <Route path="/:name" element={<Welcome/>}/>
                     <Route path="/counter" element={<NewCounter/>}/>
-                    <Route path="users/:username" element={<GithubParameter/>}/>
+                    <Route path="/users/:username" element={<GithubParameter/>}/>
                 </Routes>
             </div>
         )
